be35c278c1f0af29e4950044c5bd3f1ac6506a70
Dan Levin <dlevin64@gmail.com>
2016-05-18 09:40:28 -0400

    checkpoint 5

---
+++ dist/scripts/fixtures.js
@@ -0,0 +1,45 @@
+//==== ALBUM OBJECTS ===========================
+
+var albumPicasso = {
+    title: "The Colors",
+    artist: "Pablo Picasso",
+    label: 'Cubism',
+    year: '1981',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+         { title: 'Blue', duration: 161.71, audioUrl: '/assets/music/bloc_jams_music/blue' },
+         { title: 'Green', duration: 103.96, audioUrl: '/assets/music/bloc_jams_music/green' },
+         { title: 'Red', duration: 265.75, audioUrl: '/assets/music/bloc_jams_music/red' },
+         { title: 'Pink', duration: 143, audioUrl: '/assets/music/bloc_jams_music/pink' },
+         { title: 'Magenta', duration: 167.5, audioUrl: '/assets/music/bloc_jams_music/magenta' }
+    ]
+};
+
+var albumMarconi = {
+    title: "The Telephone",
+    artist: "Marconi",
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+         { title: 'Blue', duration: '4:26', audioUrl: '/assets/music/blue' },
+         { title: 'Green', duration: '3:14', audioUrl: '/assets/music/green' },
+         { title: 'Red', duration: '5:01', audioUrl: '/assets/music/red' },
+         { title: 'Pink', duration: '3:21', audioUrl: '/assets/music/pink' },
+         { title: 'Magenta', duration: '2:15', audioUrl: '/assets/music/magenta' }  
+    ]
+};
+
+var albumBeatles = {
+    title: "Abbey Road",
+    artist: "The Bealtes",
+    label: 'EMI',
+    year: '1969',
+    albumArtUrl: 'assets/images/album_covers/06.png',
+    songs: [
+        {title: 'Come Together', duration: '1:01'},
+        {title: 'Something', duration: '2:01'},
+        {title: 'Maxwell Silver Hammer', duration: '3:01'},
+        {title: 'Sun King', duration: '4:01'}
+    ]
+};
